85464840cef27a775a76a501c5f28bec4ae4a294
Ron Pasillas <ron@alley.com>
2025-05-26 08:35:41 -0700

    empty files for the future.

---
+++ src/features/player_game_fielding.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Feature for recording a player's hitting for a game.
+ *
+ * @package wp-roster-manager
+ */
+
+namespace Alley\WP\Roster_Manager\Features\player\game\fielding;
+
+/**
+ * Who knows what kind of stats will go here?
+ *
+ * - errors
+ * - catches
+ * - position
+ * - assists
+ */
---
+++ src/features/player_game_hitting.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Feature for recording a player's hitting for a game.
+ *
+ * @package wp-roster-manager
+ */
+
+namespace Alley\WP\Roster_Manager\Features\player\game\hitting;
+
