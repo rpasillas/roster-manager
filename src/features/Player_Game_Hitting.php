c504ef3d7deffd38aaeae9967ec7ec9634987973
Ron Pasillas <ron@alley.com>
2025-06-22 13:59:16 -0700

    Hard-code a response.

--- src/features/Player_Game_Hitting.php
+++ src/features/Player_Game_Hitting.php
@@ -33,7 +33,7 @@ final class Player_Game_Hitting implements Feature {
 	public function register_player_game_hitting_routes() {
 		register_rest_route( 'roster-manager/v1', '/player-game-hitting/', [
 			'methods'  => 'GET',
-			'callback' => [ $this, 'get_player_game_hitting' ],
+			'callback' => [ $this, 'get_player_game_batting' ],
 			'args'     => [
 				'player_id' => [
 					'required' => true,
@@ -48,7 +48,7 @@ final class Player_Game_Hitting implements Feature {
 		]);
 	}
 
-	public function permission_callback() {
+	public function permission_callback(): bool {
 		return true;
 	}
 
@@ -58,12 +58,9 @@ final class Player_Game_Hitting implements Feature {
 	 * @param \WP_REST_Request $request The REST API request.
 	 * @return \WP_REST_Response|\WP_Error
 	 */
-	public function get_player_game_hitting( \WP_REST_Request $request ): \WP_REST_Response|\WP_Error {
+	public function get_player_game_batting( \WP_REST_Request $request ): \WP_REST_Response|\WP_Error {
 		$player_id = $request->get_param( 'player_id' );
 		$game_number = $request->get_param( 'game_number' );
-
-		// Retrieve hitting stats for the player (replace with actual logic).
-		//$hitting_stats = get_option( "player_{$player_id}_hitting_stats" );
 
 		if ( ! $player_id ) {
 			return new \WP_Error(
@@ -73,10 +70,142 @@ final class Player_Game_Hitting implements Feature {
 			);
 		}
 
-		return rest_ensure_response( [
-			'player_id' => $player_id,
+		return rest_ensure_response( $this->get_player_game_batting_data( $player_id, $game_number ) );
+	}
+
+	public function get_player_game_batting_data( $id, $game_number = null ): array {
+		//@link https://en.wikipedia.org/wiki/Batted_ball
+		return [
+			'player_id'   => $id,
+			'player_name' => 'Ulises Pasillas',
+			'player_number' => 5,
 			'game_number' => $game_number,
-		] );
+			'at_bats' => 2,
+			'runs' => 1,
+			'hits' => 1,
+			'rbi' => 1,
+			'hr' => 0,
+			'opponent' => 'Red Sox',
+			'home_team' => 'Red Sox',
+			'final' => [
+				'visitors' => 6,
+				'home' => 4,
+				'result' => 'win',
+			],
+			'at_bats_pitch_sequence'     => [
+				[ // first at bat
+					'hand' => 'right',
+					'outs' => 2,
+					'rbi' => 1,
+					'pitcher' => [
+						'hand' => 'right',
+						'number' => 37,
+					],
+					'runners_on_base' => [
+						'first' => true,
+						'second' => false,
+						'third' => true,
+					],
+					'result' => 'single',
+					'pitches' => [
+						[ // first pitch
+							'count' => [
+								'balls'   => 0,
+								'strikes' => 0,
+							],
+							'result' => [
+								'batted_ball' => [
+									'fair-foul' => 'foul',
+									'characterization' => 'fly-ball', //fly-ball, pop-up, line-drive, ground-ball
+									'location' => [
+										'deep', //deep, shallow, standard
+										'left' // left, left-center, center, center-right, right, third, third-short, short, short-second
+									],
+								],
+							],
+						],
+						[ //second pitch
+							'count' => [
+								'balls'   => 0,
+								'strikes' => 1,
+							],
+							'result' => [
+								'reached_base' => '1', // a single
+								'batted_ball' => [
+									'fair-foul' => 'fair',
+									'characterization' => 'line-drive',
+									'location' => [
+										'deep',
+										'center'
+									],
+								],
+								'rbi' => 1,
+							],
+						],
+					],
+				],
+				[ // second at bat
+					'hand' => 'left',
+					'outs' => 0,
+					'pitcher' => [
+						'hand' => 'left',
+						'number' => 3,
+					],
+					'rbi'  => 0,
+					'runners_on_base' => [
+						'first' => true,
+						'second' => false,
+						'third' => false,
+					],
+					'result' => 'strikeout',
+					'pitches' => [
+						[ // first pitch
+							'count' => [
+								'balls'   => 0,
+								'strikes' => 1,
+							],
+							'result' => [
+								'strike' => 'swinging',
+							]
+						],
+						[ // second pitch
+							'count' => [
+								'balls'   => 0,
+								'strikes' => 2,
+							],
+							'result' => [
+								'strike' => 'looking',
+							]
+						],
+						[ // third pitch
+							'count' => [
+								'balls'   => 0,
+								'strikes' => 2,
+							],
+							'result' => [
+								'batted_ball' => [
+									'fair-foul' => 'foul',
+									'characterization' => 'ground-ball',
+									'location' => [
+										'standard',
+										'catcher',
+									],
+								],
+							]
+						],
+						[ // fourth pitch
+							'count' => [
+								'balls' => 0,
+								'strikes' => 2,
+							],
+							'result' => [
+								'strikeout' => 'looking'
+							]
+						],
+					],
+				],
+			]
+		];
 	}
 
 }
